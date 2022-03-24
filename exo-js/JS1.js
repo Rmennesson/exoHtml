6a18a2143949a3d24737c184a24f181323cb308a
Rmennesson <remimen@hotmail.fr>
2022-03-24 16:48:43 +0100

    exercice JS

---
+++ exo-js/JS1.js
@@ -0,0 +1,12 @@
+var name = document.getElementById('firstname').value
+var surname = document.getElementById('surname').value
+var city = document.getElementById('city').value
+
+alert ("Nom: "+surname+"Prénom: "+firstname+"Ville: "+city)
+
+
+var nom = "Mennesson \n"; 
+var prenom = "Remi \n";
+var ville = "Courbevoie\n";
+var reponse =prompt("Quel est votre prénom ?", "Saisir votre Prénom");
+alert ("Bonjour "+reponse);
